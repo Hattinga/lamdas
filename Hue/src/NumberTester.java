bfe1e152e69723a62f031a5075101be2f383c7a1
Hattinga <HattingerJ22049@sus.htl-grieskirchen.at>
2024-09-30 18:54:49 +0200

    Numbertester Klasse erstellt + Konstruktor und instanzvariablen implementiert

--- Hue/src/NumberTester.java
+++ Hue/src/NumberTester.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class NumberTester {
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+    private String fileName;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
     
 }
